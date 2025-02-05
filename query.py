231fb5027985ee9173c7bbaef39382539eab948e
rlaekdrms <danielyoonho@naver.com>
2025-02-05 21:30:22 +0900

    국내주식기간별시세 초본

--- query.py
+++ query.py
@@ -15,4 +15,24 @@ def Query_currentPrice(appkey:str,appsecret:str,token:str,code:str):
         "fid_input_iscd":code
     }
     res = requests.get(url,headers=headers,params=params)
-    return res.json()
+    
+    stock_info = {
+        "currentPrice":int(res.json()['output']['stck_oprc']),
+        "highestPrice":int(res.json()['output']['stck_hgpr']),
+        "lowestPrice":int(res.json()['output']['stck_lwpr']),
+        "maxLimit":int(res.json()['output']['stck_mxpr']),
+        "lowLimit":int(res.json()['output']['stck_llam']),
+    }   
+
+    return stock_info
+
+def GetValue_byDate(appkey:str,appsecret:str,token:str,code:str,startDate:int,endDate:int):
+    url = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice"
+
+    headers = {
+        "authorization":token,
+        "appkey":appkey
+    }
+
+    return True
+    
