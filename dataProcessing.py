6611dd99a4570cf982162b405272ca6d8da20d27
rlaekdrms <danielyoonho@naver.com>
2025-02-11 23:49:56 +0900

    숙제 업데이트 별로 못했어용

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,6 +1,14 @@
 import numpy
 
 def GetFiltered_clpr(data):
+    result = {}
     for row in data['output2']:
         print(row)
+        key = {}(row{'stock_bsop_date'})
+        result[key] = row['stck_clpr']
+        key = "20250208"
+        result["20250208"]="140000"
+        result = {
+            "20250208":"140000"
+        }
     return True
