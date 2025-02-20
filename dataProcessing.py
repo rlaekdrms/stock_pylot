2d82b2c70fe44b860f10aa161aeea6bff6fb6a30
Denver <audgkwns44@gmail.com>
2025-02-20 21:12:29 +0900

    [Update]Refactoring dataProcessing

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,19 +2,20 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import pandas as df
+
+
 def GetFiltered_clpr(data):
     columns = ["dates","values"]
-    result = {}
     df = pd.DataFrame(columns=columns)
+    
     for row in data['output2']:
         new_row =  pd.DataFrame([{"dates":int(row['stck_bsop_date']),"values":int(row['stck_clpr'])}])
         df = pd.concat([df,new_row],ignore_index=True)
-        # print(row)
-        key = row['stck_bsop_date']
-        # print(newKey)
-        result[key] = row['stck_clpr']
-    df_sorted_by_values = df.sort_values(by=["dates","values"],ascending=True)
+
+    df_sorted_by_values = df.sort_values(by=["dates"],ascending=True)
+
     plt.plot(df["values"],"orange")
     plt.show()
     print(df)
-    return result
+
+    return df
