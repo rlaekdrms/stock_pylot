5707ea58baa357beda7d02ced23f778d05316bba
Denver <audgkwns44@gmail.com>
2025-02-20 21:25:52 +0900

    [Delete ]Unnecessary sorting

--- dataProcessing.py
+++ dataProcessing.py
@@ -11,10 +11,7 @@ def GetFiltered_clpr(data):
         new_row =  pd.DataFrame([{"dates":int(row['stck_bsop_date']),"values":int(row['stck_clpr'])}])
         df = pd.concat([df,new_row],ignore_index=True)
 
-    df_sorted_by_values = df.sort_values(by=["dates"], ascending=True)
-    
     plt.plot(df["dates"], df["values"],"orange")
-    # plt.plot(df_sorted_by_values["dates"], df_sorted_by_values["values"],"b")
     plt.show()
 
     return df
